95e63e498e42092f6a9fcb4b13600f1d3d55a9bf
vhpadula <64943143+vhpadula@users.noreply.github.com>
2020-11-25 18:41:55 -0300

    commmit2

--- mapa.py
+++ mapa.py
@@ -13,14 +13,14 @@ js = np.array(f["Acidentes"][0:num],dtype=float)
 data1 = [[lat[i],lon[i],js[i]] for i in range(num)]
 
 map_osm = folium.Map(location=[-23.550,-46.650],zoom_start=11,control_scale=True)
-lay1=folium.map.FeatureGroup(name="HeatMap", overlay=True, control=True, show=True).add_to(map_osm)
-lay2=folium.map.FeatureGroup(name="Marcadoress", overlay=True, control=True, show=True).add_to(map_osm)
+lay1=folium.map.FeatureGroup(name="Marcadores", overlay=True, control=True, show=True).add_to(map_osm)
+lay2=folium.map.FeatureGroup(name="HeatMap", overlay=True, control=True, show=True).add_to(map_osm)
 HeatMap(data1,control=True).add_to(lay2)
 for i in range (num):
     pop=folium.map.Popup(html=f["Distrito"][i]+ " "+ "Acidentes:" + str(f["Acidentes"][i]))
     folium.map.Marker([lat[i],lon[i]],popup=pop).add_to(lay1)
 
 folium.map.LayerControl(position='topright', collapsed=True, autoZIndex=True).add_to(map_osm)
-file_path = r"C:/Users/vhpad/Desktop/Acidentes_de_bicicleta.html"
+file_path = r"C:/Users/vhpad/Desktop/Acidentes_de_bicicleta_version2.html"
 map_osm.save(file_path)
 webbrowser.open(file_path)
